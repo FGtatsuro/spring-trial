6eca2f3fb31c53609564d1225a475a31ed32e724
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2023-03-02 21:14:49 +0900

    Add extra attributes for Book/Author.

--- src/main/kotlin/com/trial/book/Entities.kt
+++ src/main/kotlin/com/trial/book/Entities.kt
@@ -1,9 +1,7 @@
 package com.trial.book
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import jakarta.persistence.ManyToMany
+import jakarta.persistence.*
+import java.time.LocalDate
 
 // NOTE: We should use classes, not data classes.
 //   Ref: https://github.com/spring-guides/tut-spring-boot-kotlin#persistence-with-jpa
@@ -11,6 +9,14 @@ import jakarta.persistence.ManyToMany
 class Book(
     @Id @GeneratedValue var id: Long? = null,
     var title: String,
+    var publishedDate: LocalDate? = null,
+    var description: String? = null,
+    var pageCount: Int? = 0,
+
+    // TODO: Handle book identifiers strictly
+    //   - Types(ex. ISBN_10, ISBN_13)
+    //   - Validation per types
+    @Column(unique = true) var isbn: String? = null,
 
     // NOTE: @ManyToMany may have difficult problems
     //   FYI: https://zenn.dev/dev_yoon/articles/956cbf4606d76b
@@ -21,6 +27,7 @@ class Book(
 class Author(
     @Id @GeneratedValue var id: Long? = null,
     var name: String,
+    var description: String? = null,
 
     @ManyToMany(mappedBy = "authors") var books: Set<Book> = mutableSetOf(),
 )
