bd42f5dbdcec318eae8c44933490ef4cc69061cb
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2023-03-02 22:26:55 +0900

    Add several search methods.

--- src/main/kotlin/com/trial/book/Repositories.kt
+++ src/main/kotlin/com/trial/book/Repositories.kt
@@ -1,10 +1,22 @@
 package com.trial.book
 
 import org.springframework.data.repository.CrudRepository
+import java.time.LocalDate
 
 interface BookRepository : CrudRepository<Book, Long> {
+    fun findByIsbn(isbn: String): Book
+
+    // NOTE: Performance issue may occur on growing data.
+    // Containing(=LIKE %(string)%) can't use database index.
+    fun findByDescriptionContaining(description: String): List<Book>
+
+    fun findByPublishedDateBetween(start: LocalDate, end: LocalDate): List<Book>
 }
 
 interface AuthorRepository : CrudRepository<Author, Long> {
     fun findByNameStartingWith(name: String): List<Author>
+
+    // NOTE: Performance issue may occur on growing data.
+    // Containing(=LIKE %(string)%) can't use database index.
+    fun findByDescriptionContaining(description: String): List<Author>
 }
--- src/test/kotlin/com/trial/book/RepositoriesTests.kt
+++ src/test/kotlin/com/trial/book/RepositoriesTests.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.data.repository.findByIdOrNull
+import java.time.LocalDate
 
 @DataJpaTest
 class RepositoriesTests @Autowired constructor(
@@ -53,5 +54,68 @@ class RepositoriesTests @Autowired constructor(
 
         val found = authorRepository.findByNameStartingWith("author")
         assertThat(found).isEqualTo(listOf(author1, author2))
+    }
+
+    @Test
+    fun `When findByIsbn then return a book with exact-matched isbn`() {
+        val book1 = Book(title = "book1", isbn = "12345")
+        val book2 = Book(title = "book2", isbn = "1234567")
+        entityManager.persist(book1)
+        entityManager.persist(book2)
+        entityManager.flush()
+
+        val found = bookRepository.findByIsbn("12345")
+        assertThat(found).isEqualTo(book1)
+    }
+
+    @Test
+    fun `When findByDescription(Book) then return list of books with containing condition`() {
+        val book1 = Book(title = "book1", description = "hit-suffix")
+        val book2 = Book(title = "book2", description = "prefix-hit-suffix")
+        val book3 = Book(title = "book3", description = "prefix-hit")
+        val book4 = Book(title = "book4", description = "not-match")
+        entityManager.persist(book1)
+        entityManager.persist(book2)
+        entityManager.persist(book3)
+        entityManager.persist(book4)
+        entityManager.flush()
+
+        val found = bookRepository.findByDescriptionContaining("hit")
+        assertThat(found).isEqualTo(listOf(book1, book2, book3))
+    }
+
+    @Test
+    fun `When findByDescription(Author) then return list of authors with containing condition`() {
+        val author1 = Author(name = "author1", description = "hit-suffix")
+        val author2 = Author(name = "author2", description = "prefix-hit-suffix")
+        val author3 = Author(name = "author3", description = "prefix-hit")
+        val author4 = Author(name = "author4", description = "not-match")
+        entityManager.persist(author1)
+        entityManager.persist(author2)
+        entityManager.persist(author3)
+        entityManager.persist(author4)
+        entityManager.flush()
+
+        val found = authorRepository.findByDescriptionContaining("hit")
+        assertThat(found).isEqualTo(listOf(author1, author2, author3))
+    }
+
+    @Test
+    fun `When findByPublishedDateBetween then return list of books with between condition`() {
+        val book1 = Book(title = "book1", publishedDate = LocalDate.of(2001, 1, 11))
+        val book2 = Book(title = "book2", publishedDate = LocalDate.of(2001, 1, 13))
+        val book3 = Book(title = "book3", publishedDate = LocalDate.of(2001, 2, 11))
+        val book4 = Book(title = "book4", publishedDate = LocalDate.of(2001, 2, 13))
+        entityManager.persist(book1)
+        entityManager.persist(book2)
+        entityManager.persist(book3)
+        entityManager.persist(book4)
+        entityManager.flush()
+
+        val found = bookRepository.findByPublishedDateBetween(
+            start = LocalDate.of(2001, 1, 12),
+            end = LocalDate.of(2001, 2, 12),
+        )
+        assertThat(found).isEqualTo(listOf(book2, book3))
     }
 }
